c7f0dcda4f3dd8ad1247ecdeec483bc794efba7a
PORTATIL <PORTATIL@PORTATIL-HP>
2016-03-23 12:52:21 +0100

    Hello Pill

---
+++ src/ArchivoPrueba/HelloPildora.java
@@ -0,0 +1,13 @@
+package ArchivoPrueba;
+
+public class HelloPildora {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Friend Pill");
+	}
+
+}
